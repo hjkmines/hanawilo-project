86c79fa67b37087b0db59f19ba072d44058fa763
jordanclrk74 <89047947+jordanclrk74@users.noreply.github.com>
2022-10-04 21:58:07 -0500

    task-9: implement react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
 import App from "./App";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+, document.querySelector("#root"));

804c300dee0448845a8936b554fa2e6bf9dca613
josianthawn@gmail.com <pauthawn@Josephs-MBP.attlocal.net>
2022-10-11 20:34:29 -0500

    task 9

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-
-import React from "react"; 
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Directory from "./pages/Directory";
-import AboutUs from './pages/AboutUs';
-import  Home  from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import Home from "./pages/Home";
 import MuiNavbar from "./components/MuiNavbar";
-import Footer from './components/Footer';
+import Footer from "./components/Footer";
 import { Box } from "@mui/material";
 
 function App() {
-        return(
-            <Box className='app'>
-                <MuiNavbar />
-                <Routes>
-                    <Route path='/' element={ <Home /> } />
-                    <Route path='/directory' element={ <Directory /> } />
-                    <Route path='/about' element={ <AboutUs /> } />
-                </Routes>
-                <Footer />
-            </Box>
-        )
+  return (
+    <div className="app">
+      <MuiNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/directory" element={<Directory />} />
+        <Route path="/about" element={<AboutUs />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;

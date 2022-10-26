2034bf4b5c3b5440463db1edb0d0153c247f468f
Chris Medyna <chris.medyna@gmail.com>
2022-10-26 15:56:21 -0400

    webpack configuration fixes

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,40 +1,34 @@
 const path = require("path");
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const { webpack } = require("webpack");
+// const { webpack } = require("webpack");
 
 
 module.exports = {
     mode: "development",
-    entry: "/src/index.js",
-    
+
+    entry:  './src/index.js',
     output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "bundle.js"
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js",
+        // clean: true,
+        // assetModuleFilename: '[name][ext]'
     },
-
-    devtool: "eval-cheap-source-map",
-
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: '/src/index.html'
-        }),
-    ],
-
-    target: "web",
+    devtool: "source-map",
     devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'public')
+        },
         port: "3000",
-        static: ["./src"],
         open: true,
         hot: true,
-        liveReload: true
-    },
-    resolve: {
-        extensions: [".js", ".jsx", ".json", ".ts"],
+        // compress: true,
+        // historyApiFallback: true,
+        // liveReload: true
     },
     module: {
         rules:[
             {
-                test: /.js$|jsx/,
+                test: /.js|.jsx$/i,
                 exclude: /node_modules/ , 
                 use: {
                     loader: 'babel-loader',
@@ -44,18 +38,37 @@ module.exports = {
                 }
             },
             {
-                test: /\.js$|jsx/,
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.js$|jsx/i,
+                exclude: /node_modules/,
                 enforce: "pre",
                 use: ["source-map-loader"],
             },
+            // future potential for scss loaders 
             {
                 test: /\.css$/i,
                 loader: "css-loader",
-                options: {
-                    url: true
-                }
+                // options: {
+                //     url: true
+                // }
             }
         ]
         
     },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: 'Hanawilo Team Project',
+            filename: 'index.html',
+            template: '/src/index.html'
+        }),
+    ],
+    // target: "web",
+    
+    // resolve: {
+    //     extensions: [".js", ".jsx", ".json", ".ts"],
+    // },
+    
 };

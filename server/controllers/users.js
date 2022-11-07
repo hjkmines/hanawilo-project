879e31147300e58e1f05c9bf1f7c57622ac6a5e5
James Kyan <james.kyan88@gmail.com>
2022-11-06 21:11:36 -0500

    Updated controller users

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -39,11 +39,56 @@ const deleteUsers = async (req, res, next) => {
     }
 }
 
+// Create Controller file for get, update and delte one user Joe and James
+
+//get one user
+const getOneuser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    res.status(200).setHeader("Content-type", "application/json").json(oneUser);
+  } catch (err) {
+    res.status(404).send(`Can't get user data, ${err.message}`);
+    next(err);
+  }
+};
+
+const updateOneUser = async (req, res, next) => {
+  try {
+    const userUpdate = await User.findByIdAndUpdate(
+      req.params.userId,
+      { $set: req.body },
+      { new: true, runValidators: true }
+    );
+    res
+      .status(200)
+      .setHeader("Content-Type", "application/json")
+      .json(oneUpdate);
+  } catch (err) {
+    res.status(404).send(`Can't update user data, ${err.message}`);
+    next(err);
+  }
+};
+
+//deleteOne user
+const deleteOneUser = async (req, res, next) => {
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).setHeader("Content-Tyoe", "application/json").json();
+  } catch {
+    res.status(404).send(`Can't delete user data, ${err.message}`);
+    next(err);
+  }
+};
+
+
 // Exports
 module.exports = {
     getUsers,
     createUser,
-    deleteUsers
+    deleteUsers,
+    getOneuser,
+  updateOneUser,
+  deleteOneUser
 }
 
 

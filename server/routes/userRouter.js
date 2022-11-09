55fef7b9cf8fdc7b81408a38e027b45415767286
Mai Vang <100221733+vmaineng@users.noreply.github.com>
2022-11-08 16:59:04 -0800

    Task 13 (#16)
    * routes finished
    
    * formatting routes
    
    * fixed controllers/users
    
    * updated routes
    
    * updated routes

---
+++ server/routes/userRouter.js
@@ -0,0 +1,24 @@
+const userRouter = require("express").Router();
+const {
+  getUser,
+  getUsers,
+  createUser,
+  updateOneUser,
+  deleteUsers,
+  deleteOneUser,
+} = require("../controllers/users");
+
+userRouter.route("/login").post(getUser);
+
+userRouter.route("/")
+.get(getUsers)
+.post(createUser)
+.delete(deleteUsers)
+
+userRouter
+  .route("/:userId")
+  .get(getUser)
+  .put(updateOneUser)
+  .delete(deleteOneUser);
+
+module.exports = userRouter;

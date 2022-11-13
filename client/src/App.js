106dbc823265fd08f1903b7c0fbaaaa80fdbd08e
Joseph Thawn <josianthawn@gmail.com>
2022-11-13 12:19:43 -0600

    boiler  plate for LogIn page

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,54 @@
-import React from "react"; 
-import Container from '@mui/material/Container';
-import  Box  from "@mui/material/Box";
-import './App.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { 
-    BrowserRouter,
-//     RouterProvider, 
-    Route,
-    RouterProvider,
-    Routes,
-} from 'react-router-dom';
+import React from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import {
+  BrowserRouter,
+  //     RouterProvider,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 import Directory from "./pages/Directory";
-import AboutUs from './pages/AboutUs';
-import  Home  from "./pages/Home.jsx";
-import Messages from './pages/Messages';
-import Profile from './pages/Profile'; //test for right navbar routing
+import AboutUs from "./pages/AboutUs";
+import Home from "./pages/Home.jsx";
+import Messages from "./pages/Messages";
+import Profile from "./pages/Profile"; //test for right navbar routing
 import MUI_NAVBAR from "./components/MUI_NAVBAR";
-import Footer from './components/Footer';
+import Footer from "./components/Footer";
+import Login from "./pages/Login";
 
 function App() {
-        return(
-            <div className="app" padding='0'>
-                <MUI_NAVBAR/>
-                 {/* <Container 
+  return (
+    <div className="app" padding="0">
+      <MUI_NAVBAR />
+      {/* <Container 
                  maxWidth='xl'
                  sx={{
                     marginY: 2,
                     p: 0
                  }}
                  >  */}
-                    <Routes>
-                        <Route path='/' element={ <Home /> } />
-                        <Route path='/directory' element={ <Directory /> } />
-                        {/* route for each individual within the directory - using a dynamic variable */}
-                            {/* <Route path='/directory/:id' element={ <Directory /> } /> */}
-                        <Route path='/about' element={ <AboutUs /> } />
-                        <Route path='/messages' element ={ <Messages /> } />
-                        <Route path='/profile' element={ <Profile /> } /> 
-                        {/* <Route path='*' element={<NotFound />} default error not found page */}
-                    </Routes>
-                {/* </Container > */}
-                <Footer />
-            </div>
-        )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/directory" element={<Directory />} />
+        {/* route for each individual within the directory - using a dynamic variable */}
+        {/* <Route path='/directory/:id' element={ <Directory /> } /> */}
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* <Route path='*' element={<NotFound />} default error not found page */}
+      </Routes>
+      {/* </Container > */}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/MUI_NAVBAR.js
+++ client/src/components/MUI_NAVBAR.js
@@ -1,22 +1,22 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import NorthEastOutlinedIcon from '@mui/icons-material/NorthEastOutlined';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import NorthEastOutlinedIcon from "@mui/icons-material/NorthEastOutlined";
 import { Link, NavLink } from "react-router-dom";
 
-// constants for menu/tooltip navigation 
-const pages = ['Directory', 'About', 'Messages',];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+// constants for menu/tooltip navigation
+const pages = ["Login", "Directory", "About", "Messages"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 const MUI_NAVBAR = () => {
   // constants for state -> navigation tooltips on left and right side of navbar
@@ -39,40 +39,47 @@ const MUI_NAVBAR = () => {
   };
 
   return (
-
-    // total container sticks to top of viewport via "sticky" utilizes anchor elements 
-    <AppBar position="sticky" >
-      <Box sx={{
-        padding:0,
-        margin:0,
-        width: '100%',
-        maxWidth: 'xl',
-      }} >
-        <Toolbar disableGutters sx={{ maxWidth:'xl'}}>
-
+    // total container sticks to top of viewport via "sticky" utilizes anchor elements
+    <AppBar position="sticky">
+      <Box
+        sx={{
+          padding: 0,
+          margin: 0,
+          width: "100%",
+          maxWidth: "xl",
+        }}
+      >
+        <Toolbar disableGutters sx={{ maxWidth: "xl" }}>
           {/* medium & up viewport size */}
-          <NorthEastOutlinedIcon sx={{ display: { xs: 'none', md: 'flex' }, mx: 1 }} />
+          <NorthEastOutlinedIcon
+            sx={{ display: { xs: "none", md: "flex" }, mx: 1 }}
+          />
           <Typography
             variant="h6"
             component="a"
             href="/"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'roboto',
+              display: { xs: "none", md: "flex" },
+              fontFamily: "roboto",
               fontWeight: 600,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-              fontSize:'30px'
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+              fontSize: "30px",
             }}
           >
             Hanawilo-Edge
           </Typography>
 
-            {/* less than medium viewport */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, mr:'auto' }}>
-             
+          {/* less than medium viewport */}
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "flex", md: "none" },
+              mr: "auto",
+            }}
+          >
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -87,37 +94,44 @@ const MUI_NAVBAR = () => {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
               {pages.map((page) => (
                 <MenuItem key={page} onClick={handleCloseNavMenu}>
                   <Typography textAlign="center">
-                     <Button 
+                    <Button
                       key={page}
                       component={Link}
                       to={`/${page}`}
-                      sx={{ my: 1, color: 'black', fontWeight:'500', display: 'block'}}
-                      >
-                        {page}
-                     </Button>
-                    </Typography>
+                      sx={{
+                        my: 1,
+                        color: "black",
+                        fontWeight: "500",
+                        display: "block",
+                      }}
+                    >
+                      {page}
+                    </Button>
+                  </Typography>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
-          <NorthEastOutlinedIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <NorthEastOutlinedIcon
+            sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
+          />
           <Typography
             variant="h5"
             noWrap
@@ -125,53 +139,59 @@ const MUI_NAVBAR = () => {
             href="/"
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
+              fontFamily: "monospace",
               fontWeight: 600,
-              fontSize: '17px',
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              fontSize: "17px",
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             Hanawilo-Edge
           </Typography>
-        
-        {/* buttons for navigation medium and up viewports */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+
+          {/* buttons for navigation medium and up viewports */}
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 2, color: "white", display: "block" }}
               >
-                <Link to={`/${page}`} style={{textDecoration: "none", color: "white"}}> 
-                    {page} 
+                <Link
+                  to={`/${page}`}
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  {page}
                 </Link>
               </Button>
             ))}
           </Box>
 
-              {/* avatar box - placeholder kitten image - how cute :) */}
+          {/* avatar box - placeholder kitten image - how cute :) */}
           <Box sx={{ flexGrow: 0, mr: 1 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Place Kitten" src="http://placekitten.com/g/200/300" />
+                <Avatar
+                  alt="Place Kitten"
+                  src="http://placekitten.com/g/200/300"
+                />
               </IconButton>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px' }}
+              sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}

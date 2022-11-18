a368b4512382405bc2e0689fe7d8fa5b9ec28d13
Mai Vang <vmaineng@gmail.com>
2022-11-18 11:10:01 -0800

    added image to the top

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -1,6 +1,21 @@
 import React from "react";
 import Navbar from "../components/Navbar"
 // import Footer from "../components/Footer";
+import PropTypes from 'prop-types';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+
+const mainFeaturedPost = {
+    title: 'Title of a longer featured blog post',
+    description:
+      "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+    image: 'https://source.unsplash.com/random',
+    imageText: 'main image description',
+    linkText: 'Continue reading…',
+  };
 
 const AboutUs = () => { 
     return (
@@ -8,7 +23,52 @@ const AboutUs = () => {
             <Navbar />
             <h1>this is the about us page</h1>
             {/* <Footer /> */}
-
+            <Paper
+      sx={{
+        position: 'relative',
+        backgroundColor: 'grey.800',
+        color: '#fff',
+        mb: 4,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundImage: `url(${mainFeaturedPost.image})`,
+      }}
+    >
+      {/* Increase the priority of the hero background image */}
+      {<img style={{ display: 'none' }} src={mainFeaturedPost.image} alt={mainFeaturedPost.imageText} />}
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          backgroundColor: 'rgba(0,0,0,.3)',
+        }}
+      />
+      <Grid container>
+        <Grid item md={6}>
+          <Box
+            sx={{
+              position: 'relative',
+              p: { xs: 3, md: 6 },
+              pr: { md: 0 },
+            }}
+          >
+            <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+              {mainFeaturedPost.title}
+            </Typography>
+            <Typography variant="h5" color="inherit" paragraph>
+              {mainFeaturedPost.description}
+            </Typography>
+            <Link variant="subtitle1" href="#">
+              {mainFeaturedPost.linkText}
+            </Link>
+          </Box>
+        </Grid>
+      </Grid>
+    </Paper>
         </div>
     )
 }

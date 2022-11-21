8b36fd3dd22e3f88c11c81167126244b4967eaf9
Mai Vang <vmaineng@gmail.com>
2022-11-20 16:38:46 -0800

    cleaned up

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -11,7 +11,6 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
-// import { createTheme, ThemeProvider } from "@mui/material/styles";
 import {useTheme} from "@mui/material";
 import Navbar from "../components/Navbar";
 

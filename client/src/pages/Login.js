8dba310d13f34f678578759ad3aa433904720ac2
Mai Vang <vmaineng@gmail.com>
2022-11-20 16:37:55 -0800

    updated theme for Login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -11,7 +11,9 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+// import { createTheme, ThemeProvider } from "@mui/material/styles";
+import {useTheme} from "@mui/material";
+import Navbar from "../components/Navbar";
 
 function Copyright(props) {
   return (
@@ -31,9 +33,12 @@ function Copyright(props) {
   );
 }
 
-const theme = createTheme();
+
 
 export default function Login() {
+  
+  const theme = useTheme();
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -44,7 +49,8 @@ export default function Login() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
+    <>
+    <Navbar />
       <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
         <Grid
@@ -134,6 +140,6 @@ export default function Login() {
           </Box>
         </Grid>
       </Grid>
-    </ThemeProvider>
+      </>
   );
 }

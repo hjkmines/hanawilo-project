e2ead31866ccb74e9879b7803f04a34150bcd25c
Stephanie Mitchell <stephsorandom@gmail.com>
2022-12-01 19:30:02 -0500

    task25 complete (#27)
    * task25 complete
    
    * Delete package-lock.json file

---
+++ server/controllers/getOneSkill.js
@@ -0,0 +1,48 @@
+const SkillSet = require('../models/Skillset');
+
+cont getSkill = async (req, res, next) => {
+    try { 
+        const skillSet = await SkillSet.find();
+        res
+        .status(200)
+        .setHeader('Content-Type', 'application/json')
+        .json(skillSet);
+    } catch (err) {
+        next(err);
+    }
+}
+
+const updateSkill = async (req, res, next) => {
+    try {
+        const SkillSet = await SkillSet.findByIdAndUpdate(
+            req.params.skillSetId,
+            { $set: req.body},
+            { new: true, runValidators: true }
+        );
+        res 
+            .status(200)
+            .setHeader('Content-Type', 'application/json')
+            .json(onUpdate);
+    } catch (err) {
+        res.status(404).send(`Can't update user skill, ${err.message}`)
+        next(err);
+    }
+}
+
+const deleteSkill = async (req, res, next) => {
+    try {
+        const SkillSet = await SkillSet.deleteSkill();
+        res
+        .status(200)
+        .setHeader('Content-Type', 'application/json')
+        .json(SkillSet);
+    } catch (err) {
+        next (err)
+    }
+}
+
+module.exports = {
+    getSkill,
+    updateSkill,
+    deleteSkill
+}

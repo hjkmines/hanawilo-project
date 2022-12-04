226cc6137878870c2d19b2df3bdf8244eb7c5a83
Vinh Peters <vinhpeters@outlook.com>
2022-12-03 19:38:27 -0500

    Task 19 - Refactor homepage (#26)
    * Fix trailing comma in package.json
    
    * Fix path to index.html
    
    * global theme setup, refactor navbar and footer to reflect theme
    
    * added search modal, right side navbar navigation, and more theme
    
    * Simplify palette color names and add styled components to navbar
    
    * Remove search modal from navbar
    
    * more navbar tweaks + spacing - spotify theme
    
    * more navbar fun- refactored leftside menu and navigation
    
    * Finally change menu paper color
    
    * initial profile page setup - leftside
    
    * Profile design progress, CssBasline theme support, add design font to theme run npm install
    
    * Play with margins and add magenta FPO borders, rename Home.jsx
    
    * Refactor navbar and start homepage
    
    * Adjust padding in navbar and home page
    
    * added upcoming tables
    
    * profile page touch up, navbar re-factor -> left side menu
    
    * Change table styles to match figma layout. Move graphics to asset folder.
    
    * table pagination menu color eureka
    
    * Rebase task 16
    
    * Refactor home page first attempt
    
    * chris tweaks on homepage
    
    * Make home page and navbar fully responsive
    
    * Clean up imports and padding
    
    * removed register2 from rightside navigation
    
    * Remove the debug borders and horizontal scroll caused by footer margin.
    
    * Remove old components and clean up imports
    
    Co-authored-by: Chris Medyna <chris.medyna@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,6 @@
 import React from "react";
-import Container from '@mui/material/Container';
-import Box from "@mui/material/Box";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import {
     BrowserRouter,
-    //     RouterProvider, 
     Route,
     RouterProvider,
     Routes,
@@ -27,7 +20,6 @@ function App() {
 
     return (
         <>
-
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/directory' element={<Directory />} />
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import {
-    Grid, //Grid Version 1 -> version 2 is still unstable :(
-    Stack,
-    Container, 
-    IconButton,
-    Button,
-    Box,
-    styled,
-    TextField,
-    Typography,
-    Divider,
-} from '@mui/material';
+import {styled} from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 import { Link } from 'react-router-dom';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
@@ -24,7 +22,7 @@ const Footer = () => {
 
     const StyledFooterContainer = styled(Grid)({
         display: "flex",
-        margin: 2,
+        padining: 2,
         marginTop: 6,
         justifyContent: "space-around",
         alignItems: "space-around",
@@ -37,169 +35,169 @@ const Footer = () => {
         spacing: 2
     })
 
-  return (
-    // parent container with above styling
-    <StyledFooterContainer container  >
-       {/* grid to dynamically render footer components */}
-        <Grid item xs={12} md={3} mb={2}>
-            {/* xs -> md viewport (mobile friendly) */}
-            <Box  
-                sx={{
-                    textAlign:'center', 
-                    alignContent:"center", 
-                    alignItems:"center",
-                    display: { xs:"block", md:"none"}
-                     
-            }}> 
-                <Typography mt={2} sx={{ color: theme.palette.white}} > Join Our Mailing List</Typography>
-                <br/>
-                <Divider variant="middle" sx={{bgcolor: 'white', fontWeight:500 }} />
-                <br/>
-                <Container mb={2} p={0} > 
-                    
-                        <TextField sx={{ bgcolor: "white", color:theme.palette.white}} type="text" placeholder='jdoe@gmail.com'>
-                            email 
+    return (
+        // parent container with above styling
+        <StyledFooterContainer container  >
+            {/* grid to dynamically render footer components */}
+            <Grid item xs={12} md={3} mb={2}>
+                {/* xs -> md viewport (mobile friendly) */}
+                <Box
+                    sx={{
+                        textAlign: 'center',
+                        alignContent: "center",
+                        alignItems: "center",
+                        display: { xs: "block", md: "none" }
+
+                    }}>
+                    <Typography mt={2} sx={{ color: theme.palette.white }} > Join Our Mailing List</Typography>
+                    <br />
+                    <Divider variant="middle" sx={{ bgcolor: 'white', fontWeight: 500 }} />
+                    <br />
+                    <Container mb={2} p={0} >
+
+                        <TextField sx={{ bgcolor: "white", color: theme.palette.white }} type="text" placeholder='jdoe@gmail.com'>
+                            email
                         </TextField>
-                        <Button 
+                        <Button
                             variant="contained"
                             sx={{
-                                minHeight:'3.5rem',
-                                color:theme.palette.white,
-                                bgcolor:theme.palette.darkPurple
-                            }} 
+                                minHeight: '3.5rem',
+                                color: theme.palette.white,
+                                bgcolor: theme.palette.darkPurple
+                            }}
                         // onClick={e => {
 
                         // } }
-                        > 
+                        >
                             Submit
                         </Button>
-                    
-                </Container>
-            </Box>
 
-            {/* med + viewport */}
-            <Box  
-                sx={{
-                    textAlign:'center', 
-                    alignContent:"center", 
-                    alignItems:"center",
-                    display: { xs:"none", md:"block"},
-                    mt: 2,
-                    color: theme.palette.white,
-            }}> 
-               <Stack>
-                    <Typography 
-                        textAlign={'center'} 
-                        justifyContent='center' 
-                        alignItems='center'
-                    > 
-                        Join Our Mailing List
-                    </Typography>
-                    <br />
-                    <Divider variant="middle" my={2} sx={{bgcolor: 'white', fontWeight:500}}/>
-                    <br />
-                    <TextField type="text" placeholder='JohnDoe@gmail.com' sx={{bgcolor:"white"}}>
-                        email 
-                    </TextField>
-                    <Button 
-                        size='small'
-                        variant="contained"
-                        sx={{
-                            minHeight:'3.5rem',
-                            color:theme.palette.white,
-                            bgcolor:theme.palette.darkPurple
-                        }} 
-                    // onClick={e => {
-                    // will eventually handle email subscription
-                    // } }
-                    > 
-                        Submit
-                    </Button>
-                </Stack>
-            </Box>
-        </Grid>
-        
-        {/* social links */}
-        <Grid item xs={12} md={4} mt={5}>
-            <Box sx={{ display: "block", alignContent:"center", mt:2 }}>
-                <Grid container  sx={{ alignItems:"center", justifyContent:"center"}}>
-                    <Grid item >
-                        <IconButton aria-label='facebook' >
-                            <a href="https://facebook.com" target="_blank" rel="noreferrer">
-                                <FacebookIcon  sx={{color:theme.palette.white}} fontSize='large'/>
-                            </a>
-                        </IconButton>
+                    </Container>
+                </Box>
+
+                {/* med + viewport */}
+                <Box
+                    sx={{
+                        textAlign: 'center',
+                        alignContent: "center",
+                        alignItems: "center",
+                        display: { xs: "none", md: "block" },
+                        mt: 2,
+                        color: theme.palette.white,
+                    }}>
+                    <Stack>
+                        <Typography
+                            textAlign={'center'}
+                            justifyContent='center'
+                            alignItems='center'
+                        >
+                            Join Our Mailing List
+                        </Typography>
+                        <br />
+                        <Divider variant="middle" my={2} sx={{ bgcolor: 'white', fontWeight: 500 }} />
+                        <br />
+                        <TextField type="text" placeholder='JohnDoe@gmail.com' sx={{ bgcolor: "white" }}>
+                            email
+                        </TextField>
+                        <Button
+                            size='small'
+                            variant="contained"
+                            sx={{
+                                minHeight: '3.5rem',
+                                color: theme.palette.white,
+                                bgcolor: theme.palette.darkPurple
+                            }}
+                        // onClick={e => {
+                        // will eventually handle email subscription
+                        // } }
+                        >
+                            Submit
+                        </Button>
+                    </Stack>
+                </Box>
+            </Grid>
+
+            {/* social links */}
+            <Grid item xs={12} md={4} mt={5}>
+                <Box sx={{ display: "block", alignContent: "center", mt: 2 }}>
+                    <Grid container sx={{ alignItems: "center", justifyContent: "center" }}>
+                        <Grid item >
+                            <IconButton aria-label='facebook' >
+                                <a href="https://facebook.com" target="_blank" rel="noreferrer">
+                                    <FacebookIcon sx={{ color: theme.palette.white }} fontSize='large' />
+                                </a>
+                            </IconButton>
+                        </Grid>
+                        <Grid item>
+                            <IconButton aria-label='instagram' >
+                                <a href="https://instagram.com" target="_blank" rel="noreferrer">
+                                    <InstagramIcon sx={{ color: theme.palette.white }} fontSize='large' />
+                                </a>
+                            </IconButton>
+                        </Grid>
+                        <Grid item>
+                            <IconButton aria-label='twitter'>
+                                <a href="https://twitter.com" target="_blank" rel="noreferrer">
+                                    <TwitterIcon sx={{ color: theme.palette.white }} fontSize='large' />
+                                </a>
+                            </IconButton>
+                        </Grid>
+                        <Grid item>
+                            <IconButton aria-label='github' >
+                                <a href="https://github.com" target="_blank" rel="noreferrer">
+                                    <GitHubIcon sx={{ color: theme.palette.white }} fontSize='large' />
+                                </a>
+                            </IconButton>
+                        </Grid>
+                        <Grid item>
+                            <IconButton aria-label='linked-in'>
+                                <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+                                    <LinkedInIcon sx={{ color: theme.palette.white }} fontSize='large' />
+                                </a>
+                            </IconButton>
+                        </Grid>
                     </Grid>
-                    <Grid item>
-                        <IconButton aria-label='instagram' >
-                            <a href="https://instagram.com" target="_blank" rel="noreferrer">
-                                <InstagramIcon sx={{color:theme.palette.white}} fontSize='large' />
-                            </a>
-                        </IconButton>
-                    </Grid>
-                    <Grid item>
-                        <IconButton aria-label='twitter'>
-                            <a href="https://twitter.com" target="_blank" rel="noreferrer">
-                                <TwitterIcon sx={{color:theme.palette.white}} fontSize='large' />
-                            </a>
-                        </IconButton>
-                    </Grid>
-                    <Grid item>
-                        <IconButton aria-label='github' >
-                            <a href="https://github.com" target="_blank" rel="noreferrer">
-                                <GitHubIcon sx={{color:theme.palette.white}} fontSize='large' />
-                            </a>
-                        </IconButton>
-                    </Grid>
-                    <Grid item>
-                        <IconButton aria-label='linked-in'>
-                            <a href="https://linkedin.com" target="_blank" rel="noreferrer">
-                                <LinkedInIcon sx={{color:theme.palette.white}} fontSize='large' /> 
-                            </a>
-                        </IconButton>
-                    </Grid>
-                </Grid>
-            </Box>
-        </Grid>
-      
-        
-        {/* Link Tree */}
-        <Grid item xs={12} md={3} mb={2}>
-            <Box sx={{display:"block", alignContent:"center", mt:2}}>
-                <Stack >
-                    <Button
-                        component = {Link}
-                        to='/'
-                        sx={{color:theme.palette.white}}
+                </Box>
+            </Grid>
+
+
+            {/* Link Tree */}
+            <Grid item xs={12} md={3} mb={2}>
+                <Box sx={{ display: "block", alignContent: "center", mt: 2 }}>
+                    <Stack >
+                        <Button
+                            component={Link}
+                            to='/'
+                            sx={{ color: theme.palette.white }}
                         >
                             Home
-                    </Button>
-                    <Button
-                        component = {Link}
-                        to='/about'
-                        sx={{color:theme.palette.white}}
+                        </Button>
+                        <Button
+                            component={Link}
+                            to='/about'
+                            sx={{ color: theme.palette.white }}
                         >
                             About
-                    </Button>
-                    <Button
-                        component = {Link}
-                        to='/directory'
-                        sx={{color:theme.palette.white}}
+                        </Button>
+                        <Button
+                            component={Link}
+                            to='/directory'
+                            sx={{ color: theme.palette.white }}
                         >
                             Directory
-                    </Button>
-                    <Button
-                        component = {Link}
-                        to='/messages'
-                        sx={{color:theme.palette.white}}
+                        </Button>
+                        <Button
+                            component={Link}
+                            to='/messages'
+                            sx={{ color: theme.palette.white }}
                         >
                             Messages
-                    </Button>
-                </Stack>
-            </Box>
-        </Grid>
-    </StyledFooterContainer> 
-  )
+                        </Button>
+                    </Stack>
+                </Box>
+            </Grid>
+        </StyledFooterContainer>
+    )
 }
 
 

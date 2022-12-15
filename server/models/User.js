af7bb50bebbb8d008ac1b0e316cbb55cb29c554d
Mai Vang <vmaineng@gmail.com>
2022-12-15 14:23:36 -0800

    added in subdocument to User for Rating schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,46 +1,63 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     userName: {
-        type: String,
-        unique: true,
-        required: [true, 'Please provide a user name'],
-        maxLength: [12, 'User name cannot exceed 12 characters']
+      type: String,
+      unique: true,
+      required: [true, "Please provide a user name"],
+      maxLength: [12, "User name cannot exceed 12 characters"],
     },
-    password:{
-        type: String,
-        required: [true, 'please provide a password'],
+    password: {
+      type: String,
+      required: [true, "please provide a password"],
     },
     email: {
-        type: String,
-        unique: true,
-        required: [true, 'Please provide an email address']
+      type: String,
+      unique: true,
+      required: [true, "Please provide an email address"],
     },
     firstName: {
-        type: String,
-        required: [true, 'Please provide a first name']
+      type: String,
+      required: [true, "Please provide a first name"],
     },
     lastName: {
-        type: String,
-        required: [true, 'please provide a last name']
+      type: String,
+      required: [true, "please provide a last name"],
     },
-    gender:{
-        type: String,
-        required: [true, 'please select your gender'],
-        enum: [ 
-                'Male', 
-                'Female', 
-                'Non-Binary', 
-                'Other',
-                'prefer not to answer', 
-            ]
+    gender: {
+      type: String,
+      required: [true, "please select your gender"],
+      enum: ["Male", "Female", "Non-Binary", "Other", "prefer not to answer"],
     },
     profileImage: {
-        type: String,
+      type: String,
     },
-}, {
-    timestamps: true,
-})
+    child: new mongoose.Schema({
+      rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true,
+        validate: (rating) => {
+          return typeof rating === "number";
+        },
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      tutor: {
+        type: mongoose.Schema.Types.ObjectID,
+        ref: 'User'
+      }
+    }),
+  },
 
-module.exports = mongoose.model('User', UserSchema);
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", UserSchema);

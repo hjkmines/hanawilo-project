452b818a5539705fbcbed977658a15acef5411dc
Vinh Peters <vinhpeters@outlook.com>
2023-01-03 14:11:12 -0500

    Start message schema

--- server/models/Messages.js
+++ server/models/Messages.js
@@ -1,2 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+    message: {
+        type: String,
+        required: [true, "Enter a message"],
+        unique: false
+    },
+    userName: {
+        type: String,
+        unique: true
+    }
+}, {
+    timestamps: true,
+})

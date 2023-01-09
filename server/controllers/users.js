d2bae8fc4419ff358ad805237a9f7c88199cffab
Vinh Peters <vinhpeters@outlook.com>
2023-01-09 14:58:59 -0500

    Fix typos from merge

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,15 +1,14 @@
 const User = require('../models/user');
 const passport = require('passport');
-const  authenticate  = require('../authenticate');
-const user = require('../models/user');
+const authenticate = require('../authenticate');
+
 
 
 const loginUser = async (req, res) => {
   passport.authenticate('local');
-  const token = authenticate.getToken({_id: req.body._id});
+  const token = authenticate.getToken({ _id: req.body._id });
   res.status(200)
     .setHeader('Content-Type', 'application/json')
-    .json({ success: true, token: token, status: 'You are successfully logged in!' });
     .json({ success: true, token: token, status: 'You are successfully logged in!' });
 }
 
@@ -29,38 +28,39 @@ const getUsers = async (req, res, next) => {
 
 const createUser = async (req, res, next) => {
   console.log('registering user....');
-   User.register(
-    new User({ 
-      username: req.body.username, 
-      firstName: req.body.firstName, 
+  User.register(
+    new User({
+      username: req.body.username,
+      firstName: req.body.firstName,
       lastName: req.body.lastName,
       gender: req.body.gender,
-      email: req.body.email }),
+      email: req.body.email
+    }),
     req.body.password,
-    function(err, user) {
+    function (err, user) {
       if (err) {
-       console.log('error while registering user!', err);
-       return next(err)
+        console.log('error while registering user!', err);
+        return next(err)
       } else {
         // user.save()
         console.log('user registered')
         console.log(user)
-          // err => {
-          // if (err) {
-          //     res.statusCode = 500;
-          //     res.setHeader('Content-Type', 'applicatoin/json');
-          //     res.json("second error");
-          //     return;
-          // }
-            passport.authenticate('local')(req, res, () => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json({success: true, status: 'Registration Successful!'});
-            });
-        }
+        // err => {
+        // if (err) {
+        //     res.statusCode = 500;
+        //     res.setHeader('Content-Type', 'applicatoin/json');
+        //     res.json("second error");
+        //     return;
+        // }
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({ success: true, status: 'Registration Successful!' });
+        });
       }
-   )
     }
+  )
+}
 
 const deleteUsers = async (req, res, next) => {
   try {
@@ -74,56 +74,56 @@ const deleteUsers = async (req, res, next) => {
     next(err);
   }
 }
+g
+  const getOneUser = async (req, res, next) => {
+    try {
+      const user = await User.findById(req.params.userId);
+      res.status(200).setHeader("Content-type", "application/json").json(oneUser);
+    } catch (err) {
+      res.status(404).send(`Can't get user data, ${err.message}`);
+      next(err);
+    }
+  };
 
-const getOneUser = async (req, res, next) => {
-const getOneUser = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.params.userId);
-    res.status(200).setHeader("Content-type", "application/json").json(oneUser);
-  } catch (err) {
-    res.status(404).send(`Can't get user data, ${err.message}`);
-    next(err);
+
+  const updateOneUser = async (req, res, next) => {
+    try {
+      const userUpdate = await User.findByIdAndUpdate(
+        req.params.userId,
+        { $set: req.body },
+        { new: true, runValidators: true }
+      );
+      res
+        .status(200)
+        .setHeader("Content-Type", "application/json")
+        .json(oneUpdate);
+    } catch (err) {
+      res.status(404).send(`Can't update user data, ${err.message}`);
+      next(err);
+    }
+  };
+
+  const deleteOneUser = async (req, res, next) => {
+    try {
+      await User.findByIdAndDelete(req.params.userId);
+      res.status(200).setHeader("Content-Tyoe", "application/json").json();
+    } catch {
+      res.status(404).send(`Can't delete user data, ${err.message}`);
+      next(err);
+    }
+  };
+
+
+  module.exports = {
+    loginUser,
+    getUsers,
+    createUser,
+    deleteUsers,
+    getOneUser,
+    getOneUser,
+    updateOneUser,
+    deleteOneUser
   }
-};
-
-const updateOneUser = async (req, res, next) => {
-  try {
-    const userUpdate = await User.findByIdAndUpdate(
-      req.params.userId,
-      { $set: req.body },
-      { new: true, runValidators: true }
-    );
-    res
-      .status(200)
-      .setHeader("Content-Type", "application/json")
-      .json(oneUpdate);
-  } catch (err) {
-    res.status(404).send(`Can't update user data, ${err.message}`);
-    next(err);
-  }
-};
-
-const deleteOneUser = async (req, res, next) => {
-  try {
-    await User.findByIdAndDelete(req.params.userId);
-    res.status(200).setHeader("Content-Tyoe", "application/json").json();
-  } catch {
-    res.status(404).send(`Can't delete user data, ${err.message}`);
-    next(err);
-  }
-};
-
-
-module.exports = {
-  loginUser,
-  getUsers,
-  createUser,
-  deleteUsers,
-  getOneUser,
-  getOneUser,
-  updateOneUser,
-  deleteOneUser
-}
 
 
 

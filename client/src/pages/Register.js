7623a39b6e66192c3bc3f0468183bbf6fa96fb31
Chris Medyna <chris.medyna@gmail.com>
2023-01-19 13:24:49 -0500

    react-hook-form + zod implementation on register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,8 +1,6 @@
-import React from "react";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
+import React, { useEffect } from "react";
 import {
+  Box,
   Container,
   useTheme,
   styled,
@@ -14,6 +12,8 @@ import {
   Radio,
   Button,
   Stack,
+  TextField,
+  Typography,
   Tooltip,
   Input
 } from "@mui/material";
@@ -21,44 +21,101 @@ import HElogo from "../assets/he-logo.svg";
 import Image from "mui-image";
 import Navbar from "../components/Navbar";
 import axios from "axios";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm, Controller, useController } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z, string, zodEnum } from "zod";
 
-const StyledTextField = styled(Input)(({ theme }) => ({
+
+const StyledTextField = styled(TextField)(({ theme }) => ({
   bgcolor: theme.palette.teal,
+  // component: Input,
 }));
+
+const ErrorField = styled(Typography)(({ theme }) => ({
+    // bgcolor: "white",
+    color: "red"
+}))
+
+const genders = [ "Male", "Female", "Non-Binary", "Other"];
+
+const RegisterSchema = z.object({
+  username: z.string()
+    .min(1, "User Name is required").max(15, "Maximum is 15 characters"),
+  password: z.string()
+    .min(4, "minmum password length is 4"),
+  firstName: z.string()
+    .min(1,"first name is required")
+    .max(15, "Maximum length is 15 characters"),
+  lastName: z.string()
+    .min(1, "last name is required")
+    .max(15, "Maximum length is 15 characters"),
+  email: z.string()
+    .min(1,"email is required").email("please enter a valid email address"),
+  gender: z.nativeEnum(genders, {
+    errorMap:(issue, ctx) => {
+      return {message: "please select your gender"};
+    },
+  })
+})
 
 const Register = () => {
   //use-hook-form register method
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  console.log(errors);
+  const { 
+    register, 
+    handleSubmit,  
+    control, 
+    reset,
+    formState: { errors, isSubmitSuccessful }
+  } = useForm({
+    resolver: zodResolver(RegisterSchema)
+  });
+  // const { field } = useController({ name: "gender", control});
 
   const theme = useTheme();
   const navigate = useNavigate();
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    const data = new FormData(e.currentTarget);
+  const sumbitForm = (data) => {
+    console.log(data)
+    console.log(errors)
     const user = {
-      username: data.get("User-Name"),
-      password: data.get("Password"),
-      firstName: data.get("First-Name"),
-      lastName: data.get("Last-Name"),
-      email: data.get("Email"),
-      gender: data.get("row-radio-buttons-group"),
-    };
+          username: data.username,
+          password: data.password,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          email: data.email,
+          gender: data.gender,
+        };
     axios
       .post("http://localhost:5001/user/register", { ...user })
-      .then((user) => console.log(...user));
-
-    navigate("/login");
+      .then((res) => console.log(res));
+      //await response from server -> if successful navigate to login page. If not successful than display errors / reason 
+    navigate("/login")
   };
+  // const handleFormSubmit = (e) => {
+  //   // e.preventDefault();
+  //   const data = new FormData(e.currentTarget);
+  //   const user = {
+  //     username: data.get("User-Name"),
+  //     password: data.get("Password"),
+  //     firstName: data.get("First-Name"),
+  //     lastName: data.get("Last-Name"),
+  //     email: data.get("Email"),
+  //     gender: data.get("row-radio-buttons-group"),
+  //   };
+  //   handleSubmit(user);
+  //   axios
+  //     .post("http://localhost:5001/user/register", { ...user })
+  //     .then((user) => console.log(...user));
 
+  //   navigate("/login");
+  // };
+
+  useEffect(() => {
+    if (isSubmitSuccessful){
+      reset();
+    }
+  }, [isSubmitSuccessful])
   return (
     <>
       <Navbar />
@@ -87,7 +144,7 @@ const Register = () => {
           container
           component={"form"}
           spacing={3}
-          onSubmit={handleSubmit((data) => console.log(data))}
+          onSubmit={handleSubmit(sumbitForm)}
         >
           <Grid item xs={12}>
             <Box border={2} borderColor={theme.palette.white} width="100%">
@@ -104,71 +161,103 @@ const Register = () => {
             </Box>
           </Grid>
           <Grid item xs={12} md={6}>
+            {/* <Controller
+              name="userName"
+              control={control}
+              placeholder="User-Name"
+              render={({field: { ref, ...field } }) =>{
+                <StyledTextField
+                  variant="filled"
+                  error={(errors.userName)}
+                  helperText={errors.userName.message}
+                  inputRef={ref}
+                  {...register("userName")}
+                />
+              }}
+            /> */}
             <StyledTextField
-              required
+              // required
               fullWidth
               variant="filled"
               id="outlined-required"
               label="User-Name"
               placeholder="User Name"
-              name="User-Name"
-              {...register("User-Name", {
-                required: "user name is require",
-                minLength: { value: 4, message: "min length is 4" },
-                // pattern: /asdfasdf/
-              })}
+              name="username"
+              // type="text"
+              // inputRef={register}
+              {...register("username")}
               sx={{ bgcolor: theme.palette.teal }}
             />
-            <p>{errors.firstName?.message}</p>
+            {errors.username && (
+              <ErrorField>{errors.username.message}</ErrorField>
+            )}
           </Grid>
           <Grid item xs={12} md={6}>
             <StyledTextField
-              required
+              // required
               fullWidth
               variant="filled"
               id="outlined-password-input"
-              label="Password"
+              label="password"
               placeholder="password"
-              name="Password"
+              name="password"
+              // inputRef={register}
+              {...register("password")}
               sx={{ bgcolor: theme.palette.teal }}
             />
+            {errors.password && (
+                  <ErrorField>{errors.password.message}</ErrorField>
+                )}
           </Grid>
           <Grid item xs={12} md={6}>
             <StyledTextField
-              required
+              // required
               fullWidth
               variant="filled"
               id="outlined-required"
               label="First-Name"
               placeholder="First Name"
-              name="First-Name"
+              name="firstName"
+              // inputRef={register}
+              {...register("firstName")}
               sx={{ bgcolor: theme.palette.teal }}
             />
+            {errors.firstName && (
+              <ErrorField>{errors.firstName.message}</ErrorField>
+            )}
           </Grid>
           <Grid item xs={12} md={6}>
             <StyledTextField
-              required
+              // required
               fullWidth
               variant="filled"
               id="outlined-required"
               label="Last-Name"
               placeholder="Last Name"
-              name="Last-Name"
+              name="lastName"
+              // inputRef={register}
+              {...register("lastName")}
               sx={{ bgcolor: theme.palette.teal }}
             />
+            <ErrorField>{errors.lastName?.message}</ErrorField>
           </Grid>
           <Grid item xs={12} md={6}>
             <StyledTextField
-              required
+              // required
               fullWidth
               variant="filled"
               id="outlined-required"
               label="Email"
               type="email"
-              name="Email"
+              name="email"
               placeholder="Email"
+              // inputRef={register}
+              {...register("email")}
               sx={{ bgcolor: theme.palette.teal }}
             />
+            {errors.email && (
+              <ErrorField>{errors.email.message}</ErrorField>
+            )}
           </Grid>
           <Grid
             item
@@ -181,7 +270,7 @@ const Register = () => {
               color: theme.palette.black,
             }}
           >
-            <FormControl>
+            <FormControl >
               <FormLabel
                 sx={{ display: "flex", justifyContent: "center" }}
                 id="row-radio-buttons-group-label"
@@ -192,28 +281,36 @@ const Register = () => {
                 row
                 aria-labelledby="row-radio-buttons-group-label"
                 name="row-radio-buttons-group"
+               
               >
                 <FormControlLabel
                   value="Female"
                   control={<Radio size="small" />}
                   label="Female"
+                  {...register("gender")}
                 />
                 <FormControlLabel
                   value="Male"
                   control={<Radio size="small" />}
                   label="Male"
+                  {...register("gender")}
                 />
                 <FormControlLabel
                   value="Non-Binary"
                   control={<Radio size="small" />}
                   label="Non-Binary"
+                  {...register("gender")}
                 />
                 <FormControlLabel
                   value="Other"
                   control={<Radio size="small" />}
                   label="Other"
+                  {...register("gender")}
                 />
               </RadioGroup>
+              {errors.gender && (
+              <ErrorField>{errors.gender.message}</ErrorField>
+            )}
             </FormControl>
           </Grid>
           <Grid item xs={12}>
@@ -243,7 +340,7 @@ const Register = () => {
                     }}
                     type="submit"
                   >
-                    Submit
+                    Register
                   </Button>
                 </Container>
               </Grid>
@@ -259,7 +356,7 @@ const Register = () => {
                   }}
                 >
                   {/* <Box sx={{ display:"flex", justifyContent:"center", alignItems:"center", justifyItems:"center", alignText:"center"}}> */}
-                  <Typography fontWeight={500} fontSize={12}>
+                  <Typography fontWeight={500} fontSize={12} color="gray">
                     Already a member?
                   </Typography>
                   {/* </Box> */}
